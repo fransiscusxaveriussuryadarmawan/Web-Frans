8bffb803946d163e6f102f70efb1ece0c10c7f98
fransiscusxaveriussuryadarmawan <s160422003@student.ubaya.ac.id>
2024-11-01 01:36:27 +0700

    TEAM F48 TITIK 1

--- canva.php
+++ canva.php
@@ -89,16 +89,16 @@ $csrfToken = "YM2OIKfwWytVKoQ3tAuDuYLtjEfc6Oo3jotAwza1";
                     <div class="row g-5 align-items-end">
                         <div class="col-lg-6 text-center text-lg-start">
 
-                            <h1 class="text-white mb-4 animated slideInDown">Version Canva Pro (30 Days) --- Expired 30
-                                November 2024 Premium<br> ⭐⭐⭐⭐⭐</h1>
-                            <p class="text-white pb-3 animated slideInDown">SPECIAL OKTOBER 2024 (JADI KLIK OTOMATIS
+                            <h1 class="text-white mb-4 animated slideInDown">Version Canva Pro (30 Days) --- Expired 1
+                                December 2024 Premium<br> ⭐⭐⭐⭐⭐</h1>
+                            <p class="text-white pb-3 animated slideInDown">SPECIAL NOVEMBER 2024 (JADI KLIK OTOMATIS
                                 MASUK
                                 TEAM PREMIUM) dengan begitu bisa akses semua fitur seperti unlock all templates,
                                 effects, dan lainnya. Thank You For Your Support !!</p>
-                            <a href="https://sub4unlock.io/KGAzd" class="btn btn-secondary py-sm-3 px-sm-5 rounded-pill me-3 animated slideInLeft">Click
+                            <a href="https://sub4unlock.io/dS4RV" class="btn btn-secondary py-sm-3 px-sm-5 rounded-pill me-3 animated slideInLeft">Click
                                 This For Join</a>
-                            <a href="https://sub4unlock.io/KGAzd" class="btn btn-light py-sm-3 px-sm-5 rounded-pill animated slideInRight">SPECIAL TEAM 47
-                                [17]</a>
+                            <a href="https://sub4unlock.io/dS4RV" class="btn btn-light py-sm-3 px-sm-5 rounded-pill animated slideInRight">SPECIAL TEAM 47
+                                [1]</a>
                         </div>
                         <div class="col-lg-6 text-center text-lg-start">
                             <img class="img-fluid animated zoomIn" src="img/hero.png" alt="">
