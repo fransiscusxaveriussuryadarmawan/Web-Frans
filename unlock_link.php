79894cf7498ffd8c78d27044432c5f34a583ecff
Fransiscus Xaverius Surya Darmawan <s160422003@student.ubaya.ac.id>
2024-11-08 10:12:42 +0700

    Update unlock_link.php

--- unlock_link.php
+++ unlock_link.php
@@ -240,7 +240,7 @@ header("Pragma: no-cache");
         </div>
 
         <div class="card">
-            <h1>Unlock Exclusive Content (F48 TITIK 50)</h1>
+            <h1>Unlock Exclusive Content (F48 TITIK 51)</h1>
             <p>Complete all steps below to access the exclusive content!</p>
 
             <div id="actions">
@@ -433,7 +433,7 @@ header("Pragma: no-cache");
 
         function unlockLink() {
             sessionStorage.setItem('actionsCompleted', 'true');
-            window.location.href = 'https://www.canva.com/brand/join?token=SNFmg4pPZ3WqtQjGt-yhFg&referrer=team-invite';
+            window.location.href = 'https://www.canva.com/brand/join?token=8TzhWY5Kc7M-7nJvU3-FQQ&referrer=team-invite';
         }
 
         window.onload = function() {
