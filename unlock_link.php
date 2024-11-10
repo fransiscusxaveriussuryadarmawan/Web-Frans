a60d395465660d8b989af266de31f9122f64d02f
fransiscusxaveriussuryadarmawan <s160422003@student.ubaya.ac.id>
2024-11-10 08:57:02 +0700

    Update unlock_link.php

--- unlock_link.php
+++ unlock_link.php
@@ -242,7 +242,7 @@ require 'visitors.php';
         </div>
 
         <div class="card">
-            <h1>Unlock Exclusive Content (F48 TITIK 60)</h1>
+            <h1>Unlock Exclusive Content (F48 TITIK 61)</h1>
             <p>Complete all steps below to access the exclusive content!</p>
 
             <div id="actions">
@@ -439,7 +439,7 @@ require 'visitors.php';
 
         function unlockLink() {
             sessionStorage.setItem('actionsCompleted', 'true');
-            window.location.href = 'https://www.canva.com/brand/join?token=dJD8HKzf_8aEDEbgSo4Jeg&referrer=team-invite';
+            window.location.href = 'https://www.canva.com/brand/join?token=NJIDL-C6ohgn4uDLquu9pw&referrer=team-invite';
         }
 
         window.onload = function() {
